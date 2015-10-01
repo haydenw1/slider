7af5ffa609e9f6fa9f322dd8a678072bc82b15b0
Hayden Wagner <hbw6955@rit.edu>
2015-10-01 01:17:48 -0400

    Beta release
    1. changed file structure to have json, assets, and css in own folders.
    2. can now make multiple sliders on one page successfully
    3. d3 dots added
    4. transitions between images eliminated for first beta release
    5. Slider now sizes well (although will need media queries for small and
    large screen sizes for the navs, arrows, dots and text atleast.
    6. small changes to text shadows
    7. changes to the width of nav areas

---
+++ json/sliderJSON.js
@@ -0,0 +1,31 @@
+sliderJSON = {
+  test:[
+    {
+      "desc":"This it the first test description text.",
+      "image":"test1.jpg",
+      "title":"Test Project 1"
+    },
+    {
+      "desc":"This is the second test decription text.",
+      "image":"test2.jpg",
+      "title":"Test Project 2"
+    },
+    {
+      "desc":"This is the third test decription text.",
+      "image":"test3.jpg",
+      "title":"Test Project 3"
+    }
+  ],
+  test1:[
+    {
+      "desc":"This it the first test description text.",
+      "image":"test1.jpg",
+      "title":"Test Project 1"
+    },
+    {
+      "desc":"This is the second test decription text.",
+      "image":"test2.jpg",
+      "title":"Test Project 2"
+    }
+  ]
+};
